74c59a14701555b9cb052f46ee44e284dabc1d34
zen <didehdrb21@naver.com>
2025-08-21 23:29:51 +0900

    add css for debug

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// 개발 환경에서만 디버깅용 border 스타일 로드
+if (import.meta.env.DEV) {
+  import('./debug.css')
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
